1b82e1964c3000e8bb55384b35b77006f3505aaf
Nand Patel <nand.pats@gmail.com>
2024-01-21 08:00:25 -0800

    fix icons

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,24 +1,33 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Navigation() {
   return (
-    <div className="navigation">
-      <img 
-        src={require('../../public/images/icons/events.svg')} 
-        alt="Plans" 
-      />
-      <img
-        src={require('../../public/images/icons/add.svg')}
-        alt="Create plan"
-      />
-      <img
-        src={require('../../public/images/icons/profile.svg')}
-        alt="Profile"
-      />
-      <img
-        src={require('../../public/images/icons/settings.svg')}
-        alt="Settings"
-      />
+    <div className="w-full h-11 px-12 py-1.5 bg-gray-200 justify-center items-start gap-14 inline-flex">
+    <Image
+      src="/images/icons/events.svg"
+      alt="Plans"
+      width={32}
+      height={32}
+    />
+    <Image
+      src="/images/icons/add.svg"
+      alt="Create plan"
+      width={32}
+      height={32}
+    />
+    <Image
+      src="/images/icons/profile.svg"
+      alt="Profile"
+      width={32}
+      height={32}
+    />
+    <Image
+      src="/images/icons/settings.svg"
+      alt="Settings"
+      width={32}
+      height={32}
+    />
     </div>
   )
 }
