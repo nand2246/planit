c8d65a09f47f73365ec64c5b6da81d0f83bf7a0b
Nand Patel <nand.pats@gmail.com>
2024-01-21 09:17:51 -0800

    add landing page and fonts

--- src/app/page.js
+++ src/app/page.js
@@ -3,19 +3,44 @@
 import React from 'react'
 import Image from 'next/image.js'
 import ProfileClient from '../components/User.jsx'
+import OpaqueButton from '@/components/buttons/OpaqueButton.jsx'
+
+import { Amaranth } from 'next/font/google'
+
+const amaranth = Amaranth({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400'
+})
 
 export default function Home() {
   return (
     <>
-    <Image
+    <div>
+      <Image
       src="/Gradient.png"
       layout='fill'
       objectFit='cover'
       alt="Picture of the author"
-    />      <a href="/api/auth/signup">Sign up</a>
-      <a href="/api/auth/login">Login</a>
-      <a href="/api/auth/logout">Logout</a>
-      <ProfileClient></ProfileClient>
+      style={{
+        objectFit: 'cover',
+        zIndex: -1
+      }}
+      />
+      <div className='w-screen items-start justify-end flex p-10'>
+          <a href="/api/auth/signup">
+            <OpaqueButton text={"Sign Up"}/>
+          </a>
+          <a href="/api/auth/login">        
+            <OpaqueButton text={"Login"}/>
+          </a>
+      </div>
+      <div className='w-screen h-screen items-center justify-center flex'>
+        <div className={"w-80 h-72 w-fit text-stone-400 align-center justify-center text-8xl font-bold font-['Amaranth'] " + amaranth.className}>Make a <br/> plan,  <br/>make a <br/>friend.</div>
+      </div>
+
+    </div>
+
     </>
   )
 }
