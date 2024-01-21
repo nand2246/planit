37c0e1bb17326cbda453a622769f9f183ab1e925
Nand Patel <nand.pats@gmail.com>
2024-01-21 07:43:47 -0800

    add background image

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
 'use client'
 
 import React from 'react'
+import Image from 'next/image.js'
 import ProfileClient from '../components/User.jsx'
 
 export default function Home() {
   return (
     <>
-      <a href="/api/auth/signup">Sign up</a>
+    <Image
+      src="/Gradient.png"
+      layout='fill'
+      objectFit='cover'
+      alt="Picture of the author"
+    />      <a href="/api/auth/signup">Sign up</a>
       <a href="/api/auth/login">Login</a>
       <a href="/api/auth/logout">Logout</a>
       <ProfileClient></ProfileClient>
